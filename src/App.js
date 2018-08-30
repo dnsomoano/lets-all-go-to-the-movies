6a857be6f54a410770fe1f169d85a39ac0eca19d
Daniel N Somoano <dnsomoano@gmail.com>
2018-08-30 15:35:18 -0400

    fetched json into console log

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+// import {BrowserRouter as Router, Link, Switch} from "react-router-dom";
+import "./Styling/App.css";
+
+// const BASE_URL = "https://api.themoviedb.org/3/movie";
+// const NOW_PLAYING = "/now_playing?api_key=";
+// const API_KEY = "e99344bac0d2a5336621a8492eeb2e74";
+// const FORMAT = "&language=en-US&page=1";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: []
+    };
+  }
+  componentDidMount() {
+    fetch(
+      "https://api.themoviedb.org/3/movie/now_playing?api_key=e99344bac0d2a5336621a8492eeb2e74&language=en-US&page=1"
+    )
+      .then(resp => {
+        if (resp.status === 200) {
+          return resp.json();
+        } else {
+          return <section>404</section>;
+        }
+      })
+      .then(json => {
+        console.log(json);
+        this.setState({
+          movies: json
+        });
+      });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <header className="App-header">Now showing</header>
+        <section className="movie-list" />
       </div>
     );
   }
